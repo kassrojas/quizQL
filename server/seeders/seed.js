35e7bb3cf14496ee422d69b8024dcb85dfa88925
Daniel Bartolini <djb3149@gmail.com>
2023-02-11 16:11:13 -0500

    added some seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,7 +1,8 @@
 const db = require('../config/connection');
-const { User, Category } = require('../models');
+const { User, Category, Quiz } = require('../models');
 const userSeeds = require('./userSeeds.json');
 const categorySeeds = require('./categorySeeds.json');
+const quizSeeds = require('./quizSeeds.json');
 
 
 db.once('open', async () => {
@@ -10,6 +11,8 @@ db.once('open', async () => {
     await User.create(userSeeds);
     await Category.deleteMany({});
     await Category.create(categorySeeds);
+    await Quiz.deleteMany({});
+    await Quiz.create(quizSeeds);
   } catch (err) {
     console.error(err);
     process.exit(1);
