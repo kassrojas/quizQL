fd1c7d2d892b8801e868213cefd75f186aac725e
Smoke5643 <smoke5643@gmail.com>
2023-02-13 15:24:51 -0500

    added to footer, about and contact

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -5,12 +5,15 @@ import githubLogo from '../../assets/githubLogo.png'
 const Footer = () => {
   return (
     <footer className="fixed-bottom text-center bg-secondary">
-      <div className='row align-items-center'>
-        <div className='col-2'>
-          <Link to="/contact">Contact Us</Link>
+      <div className='row align-items-center text-center justify-content-end'>
+        <div className='col-1'>
+          <Link className="text-light" to="/about">About Us</Link>
         </div>
-        <div className="col-2 d-flex flex-end m-2 bg-image">
-          <a href="https://github.com/kassrojas/quizQL" className="" target="_blank" rel="noreferrer"><img src={githubLogo} alt="Github Logo" height={50} /></a>
+        <div className='col-1'>
+          <Link className="text-light" to="/contact">Contact Us</Link>
+        </div>
+        <div className="col-1 d-flex flex-end m-2 bg-image">
+          <a href="https://github.com/kassrojas/quizQL" className="" target="_blank" rel="noreferrer"><img src='../../assets/githubLogo.png' alt="Github Logo" height={50} /></a>
         </div>
       </div>
     </footer>
