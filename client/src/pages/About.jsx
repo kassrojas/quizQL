911432939437e547a095b296c8c8bc64c60e23f9
Smoke5643 <smoke5643@gmail.com>
2023-02-13 15:24:51 -0500

    added to footer, about and contact

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 
 const About = () => {
 
-return <h1 className='text-white'>Test</h1>
+    return (
+        <div className='container'>
+            <div className='row justify-content-center'>
+                <div className='col-6 my-5 text-center'>
+                    <h1 className='text-white'>About Quiz QL</h1>
+                </div>
+            </div>
+            <div className='row justify-content-center'>
+                <div className='col-6 my-5 text-center'>
+                    <p className='text-white fs-4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo velit ipsam, 
+                    voluptate officiis facilis quo debitis natus dolorem quasi cupiditate doloremque. Dolorum dignissimos rerum 
+                    eius! Dolor aspernatur laboriosam quod reiciendis?</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default About;
