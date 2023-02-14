a8992d3f01424eb53d8759bb5081c0ca98bfa97a
Smoke5643 <smoke5643@gmail.com>
2023-02-14 11:05:35 -0500

    changed Quiz and Questions components

--- client/src/pages/Quiz.jsx
+++ client/src/pages/Quiz.jsx
@@ -4,7 +4,12 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 // Utilities
 import { QUERY_QUESTIONS } from '../utils/queries';
+import Question from "./Question";
 
+
+// const handleAnswer = () => {
+
+// }
 
 const Quiz = () => {
   const { topic } = useParams();
@@ -15,16 +20,6 @@ const Quiz = () => {
   });
   const questions = data?.searchQuestions || {};
 
-  console.log(category);
-  console.log(questions);
-  
-  const [question, setQuestion] = useState(questions[0].question);
-  const [answerA, setAnswerA] = useState(questions[0].correct_answer);
-  const [answerB, setAnswerB] = useState(questions[0].incorrect_answers[0]);
-  const [answerC, setAnswerC] = useState(questions[0].incorrect_answers[1]);
-  const [answerD, setAnswerD] = useState(questions[0].incorrect_answers[2]);
-  const [snippet, setSnippet] = useState(questions[0].snippet);
-
   if (loading) {
     return (
       <main>
@@ -34,26 +29,10 @@ const Quiz = () => {
   }
 
   return (
-    <main>
-     <div className="card">
-        <div className="card-body">
-          <h4>{question}</h4>
-        </div>
-        <img src={snippet} />
-        <div>
-          <ul>
-            <li>{answerA}</li>
-            <li>{answerB}</li>
-            <li>{answerC}</li>
-            <li>{answerD}</li>
-          </ul>
-          {/* <button onClick={handleAnswer} className="btn btn-primary" type="submit">
-            Submit
-          </button> */}
-        </div>
-      </div>
-    </main>
-    
+    <>
+   <Question questions={questions} />
+   <button type="button" class="btn btn-primary">Submit</button>
+   </>
   )
 }
 
