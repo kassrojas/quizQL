34838bdc0c2f5a0685e8f25f4f60586cd1cbfa67
kassrojas <kassandrarojass@gmail.com>
2023-02-15 21:04:37 -0500

    styling on leaderboard page and restults page

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import githubLogo from '../../assets/githubLogo.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import githubLogo from "../../assets/githubLogo.png";
 
 const Footer = () => {
   return (
     <footer className="fixed-bottom text-center bg-secondary">
-      <div className='row align-items-center text-center justify-content-end'>
-        <div className='col-1'>
-          <Link className="text-light" to="/about">About Us</Link>
+      <div className="row align-items-center text-center justify-content-end">
+        <div className="col-1">
+          <Link className="text-light" to="/about">
+            About Us
+          </Link>
         </div>
-        <div className='col-1'>
-          <Link className="text-light" to="/contact">Contact Us</Link>
+        <div className="col-1">
+          <Link className="text-light" to="/contact">
+            Contact Us
+          </Link>
         </div>
         <div className="col-1 d-flex flex-end m-2 bg-image">
-          <a href="https://github.com/kassrojas/quizQL" className="" target="_blank" rel="noreferrer"><img src={githubLogo} alt="Github Logo" height={50} /></a>
+          <a
+            href="https://github.com/kassrojas/quizQL"
+            className=""
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={githubLogo} alt="Github Logo" height={50} />
+          </a>
         </div>
       </div>
     </footer>
