6e5494443fc56ac6beab1c6a26a00df76f61f9b7
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-02-15 21:50:08 -0500

    work on about page

--- client/src/components/About/index.jsx
+++ client/src/components/About/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import './index.css';
 
 const About = () => {
   return (
-    <div className="container">
+    <div className="container-about">
       <div className="row justify-content-center">
         <div className="col-6 text-center">
           <h2 className="text-white my-4">About Us</h2>
